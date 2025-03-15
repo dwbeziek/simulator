befa1147f63a1d2965c46d079c91f799920d4d7f
dwbeziek <dwbeziek@gmail.com>
2025-03-15 11:09:12 +0200

    *the imei can be save as attributes when you create the device

--- RuleChain/save_attributes.js
+++
@@ -1,5 +0,0 @@
-var topic = metadata.topic; // Get the MQTT topic
-var imei = topic.split("/")[1]; // Extract IMEI from topic structure
-
-metadata.imei = imei; // Attach IMEI to metadata
-return { metadata: metadata, msg: msg };
